36e8d73c00d52264bb114f269823bc271b5d639b
Eduardo Vazquez Kuri <eduardovkuri@ciencias.unam.mx>
2024-10-23 15:34:25 -0600

    aaaaaaaaaaaaaa

--- proj_1_module.py
+++ proj_1_module.py
@@ -3,32 +3,35 @@ def euler(f,f0,n,h):
     t = 0
     b = n*h
     s0 = f0
-    s = []
+    s, T = [],[]
     while t <= b:
         ssq = s0 + h*f(t, s0)
         s.append(ssq)
+        T.append(t)
         s0 = ssq
         t += h
-    return s
+    return s, T
 
 def rkuta2(f,f0,n,h):
     t = 0
     b = n*h
     s0 = f0
-    s = []
+    s,T = [],[]
     while t<=b:
         k1n = h*f(t, s0)
         k2n = h*f(t, s0 + 0.5*k1n)
         s.append(s0 + k2n)
+        T.append(t)
         s0 += k2n
         t += h
-    return s
+    return s, T
 
 def rkuta4(f,f0,n,h):
     t = 0
     b = n*h
     s0 = f0
-    s = []
+    s,T = [],[]
+
     while t<=b:
         k1n = h*f(t, s0)
         k2n = h*f(t, s0 + 0.5*k1n)
@@ -36,9 +39,11 @@ def rkuta4(f,f0,n,h):
         k4n = h*f(t, s0 + k3n)
         si = s0 + k1n/6 + k2n/3 + k3n/3 + k4n/6
         s.append(si)
+        T.append(t)
         s0 = si
+
         t += h
-    return s
+    return s, T
 
 def SEuler(f,f0,n,h):
     return 0
@@ -46,7 +51,7 @@ def SEuler(f,f0,n,h):
 def StormerV(f,f0,n,h):
     return 0
 
-def dynamics_solve(f, f0, h, n):
+def dynamics_solve(f, f0 = 1, h = 0.1, n = 100,d = 1, t0 = 0 method = 0):
     method = input("please enter a number to choose your method (0 = Euler, 1 = Runge Kutta 2, 2 = Runge Kutta 4)")
     if int(method) == 0:
         return euler(f, f0, n, h)
@@ -56,7 +61,7 @@ def dynamics_solve(f, f0, h, n):
         return rkuta4(f, f0, n, h)
     else:
         return None
-def hamiltonian_solve(f, f0 ,n ,h):
+def hamiltonian_solve(f, f0 = 1, h = 0.1, n = 100,d = 1, t0 = 0, method = 0):
     method = input("please enter a number to choose your method (0 = Euler, 1 = Runge Kutta 2, 2 = Runge Kutta 4, 3 = Symplectic Euler, 4 = Stormer-Verlet)")
     if int(method) == 0:
         return euler(f, f0, n, h)
