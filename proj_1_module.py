9d0ff96655d191611ca5505631ff8411791d1178
Eduardo Vazquez Kuri <eduardovkuri@ciencias.unam.mx>
2024-10-23 15:16:13 -0600

    Started Hamiltonian_solver directing the user to SymplecticEuler & Stormer Verlet

--- proj_1_module.py
+++ proj_1_module.py
@@ -40,19 +40,36 @@ def rkuta4(f,f0,n,h):
         t+=1
     return s
 
+def SEuler(f,f0,n,h):
+    return 0
+
+def StormerV(f,f0,n,h):
+    return 0
 
 def dynamics_solve(f, f0, h, n):
     method = input("please enter a number to choose your method (0 = Euler, 1 = Runge Kutta 2, 2 = Runge Kutta 4)")
     if int(method) == 0:
-        return euler(f,f0,n,h)
+        return euler(f, f0, n, h)
     elif int(method) == 1:
-        return rkuta2(f,f0,n,h)
+        return rkuta2(f, f0, n, h)
     elif int(method) == 2:
-        return rkuta4(f,f0,n,h)
+        return rkuta4(f, f0, n, h)
     else:
         return None
-def hamiltonian_solve():
-    return 0
+def hamiltonian_solve(f, f0 ,n ,h):
+    method = input("please enter a number to choose your method (0 = Euler, 1 = Runge Kutta 2, 2 = Runge Kutta 4, 3 = Symplectic Euler, 4 = Stormer-Verlet)")
+    if int(method) == 0:
+        return euler(f, f0, n, h)
+    elif int(method) == 1:
+        return rkuta2(f, f0, n, h)
+    elif int(method) == 2:
+        return rkuta4(f, f0, n, h)
+    elif int(method) == 3:
+        return SEuler(f, f0, n, h)
+    elif int(method) == 4:
+        return StormerV(f, f0, n, h)
+    else:
+        return None
 a = dynamics_solve(lambda t,x : np.exp(-x*t), 1,5e-5,100)
 
 print(a)
