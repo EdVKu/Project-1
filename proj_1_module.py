62cd7856cfb3082bd5b1efbdabbbd89a98887ae1
Eduardo Vazquez Kuri <eduardovkuri@ciencias.unam.mx>
2024-10-25 19:25:59 -0600

    changed a bunch of stuff part 2

--- proj_1_module.py
+++ proj_1_module.py
@@ -1,7 +1,7 @@
 import numpy as np
-def euler(f,f0,n,h):
-    t = 0
-    b = n*h
+def euler(f,f0,n,h,t_o):
+    t = t_o
+    b = h*(n+1)
     s0 = f0
     s, T = [],[]
     while t <= b:
@@ -14,7 +14,7 @@ def euler(f,f0,n,h):
 
 def rkuta2(f,f0,n,h):
     t = 0
-    b = n*h
+    b = h*(n+1)
     s0 = f0
     s,T = [],[]
     while t<=b:
@@ -25,10 +25,12 @@ def rkuta2(f,f0,n,h):
         s0 += k2n
         t += h
     return s, T
-
+def T(n,h,t_o = 0):
+    dt = h*(n)-t_o
+    return [i*dt for i in range(n+1)]
 def rkuta4(f,f0,n,h):
     t = 0
-    b = n*h
+    b = h*(n+1)
     s0 = f0
     s,T = [],[]
 
@@ -46,40 +48,37 @@ def rkuta4(f,f0,n,h):
     return s, T
 
 def SEuler(f,g,f0,n,h):
-    p, q = [], []
-    return p, q
+    p, q, T = np.zeros(n), np.zeros(n), []
+
+
+    return p, q, T
 
 def StormerV(f,g,f0,n,h):
-    p, q = [], []
+    p, q, T = [], [], []
 
-    return p, q
+    return p, q, T
 
 def dynamics_solve(f, f0 = 1, h = 0.1, n = 100, D = 1, t0 = 0, method = "Euler"):
-    if method == "Euler":
-        vsol = [euler(f[0], f0, n, h)[1]]
-        for i in range(D):
-            vsol.append(euler(f[i], f0, n, h)[0])
-        return np.array(vsol)
-    elif method == "RK2":
-        vsol = [rkuta2(f[0], f0, n, h)[1]]
-        for i in range(D):
-            vsol.append(rkuta2(f[i], f0, n, h)[0])
-        return np.array(vsol)
-    elif method == "RK4":
-        vsol = [rkuta4(f[0], f0, n, h)[1]]
-        for i in range(D):
-            vsol.append(rkuta4(f[i], f0, n, h)[0])
-        return np.array(vsol)
-    else:
+    method_dict = {
+        "Euler": euler,
+        "RK2": rkuta2,
+        "RK4": rkuta4
+    }
+    if method not in method_dict:
         return None
+    
+    selected_method = method_dict[method]
+    vsol = T(n,h,t0)
+    vsol.extend(selected_method(f[i], f0, n, h)[0] for i in range(D))
+    return np.array(vsol)
 def hamiltonian_solve(f,g, f0 = 1, h = 0.1, n = 100, D = 1, t0 = 0, method = "Euler"):
     if method != "SE" or method != "SV":
-        dynamics_solve(f, f0, h, n, D, t0, method)
+        for i in range(D):
+            vsol = [dynamics_solve(f[i],)]
     elif method == "SE":
         vsol = [SEuler(f[0], g[0], f0, n, h)[1]]
         for i in range(D):
             vsol.append(SEuler(f[i], g[i], f0, n, h)[0])
-        vsol.appen()
         return np.array(vsol)
     elif method == "SV":
         vsol = [StormerV(f[0], g[0], f0, n, h)[1]]
